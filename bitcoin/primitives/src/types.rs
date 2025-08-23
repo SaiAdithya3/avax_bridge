43ced67222346cc48a70525ea431773d1ed7a960
raagan-u <n0r313n7@protonmail.ch>
2025-08-23 12:51:48 +0530

    add working bitcoin htlc; cargo inits

---
+++ bitcoin/primitives/src/types.rs
@@ -0,0 +1,15 @@
+use bitcoin::{ScriptBuf, Witness};
+
+pub struct BitcoinTaprootWitnessStack {
+    pub signature: Vec<u8>,
+    pub secret: Option<Vec<u8>>,
+    pub random_sig: Option<Vec<u8>>,
+    pub script: ScriptBuf,
+    pub control_block: Vec<u8>,
+}
+
+impl BitcoinTaprootWitnessStack {
+    pub fn new(signature: Vec<u8>, secret: Option<Vec<u8>>, random_sig: Option<Vec<u8>>, script: ScriptBuf, control_block: Vec<u8>) -> Self {
+        Self { signature, secret, random_sig, script, control_block }
+    }
+}
